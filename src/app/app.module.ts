203dc27804697cf14aca1e602ecda8c072572c55
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-01 01:36:05 -0300

    Dealing with Missing Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 
 import { PhotosModule } from './photos/photos.module';
 
@@ -13,7 +14,8 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     PhotosModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
