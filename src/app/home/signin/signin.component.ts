1273d6af2e0e2c6399bd32068593a3ad2e82bdae
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-01 19:07:15 -0300

    Sending Credentials to the API

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from 'src/app/core/auth.service';
+
 @Component({
   templateUrl: './signin.component.html'
 })
@@ -8,12 +10,30 @@ export class SignInComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private authService: AuthService) { }
 
   ngOnInit(): void {
+
     this.loginForm = this.fb.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login() {
+
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService
+      .authenticate(userName, password)
+      .subscribe({
+        next: () => console.log('authenticated'),
+        error: (err) => {
+          console.log(err);
+          this.loginForm.reset();
+          alert('Invalid user name or password');
+        }
+      });
   }
 }
