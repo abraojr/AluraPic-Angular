82e2c5a51f249ba07ebde4860feb464b4bcc39bb
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-01 18:24:45 -0300

    Login Form Validation

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './signin.component.html'
 })
-export class SignInComponent { }
+export class SignInComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+}
