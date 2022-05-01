3f39ea4202e033586c7597da3c65ec796f8eceeb
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-01 03:15:31 -0300

    Using RxJS and implementing debounceTime in the filter

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime, Subject } from 'rxjs';
 
 import { Photo } from '../photo/photo';
 
@@ -8,10 +9,11 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {
   }
@@ -19,6 +21,12 @@ export class PhotoListComponent implements OnInit {
   ngOnInit(): void {
 
     this.photos = this.activatedRoute.snapshot.data['photos'];
+
+    this.debounce.pipe(debounceTime(300)).subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
   applyFilter(event: Event): string {
