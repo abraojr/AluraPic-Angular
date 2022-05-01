d233420116918abd3186dc5eae06861ab84cff35
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-01 17:32:04 -0300

    Implementing Custom Directives

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CardModule } from 'src/app/shared/components/card/card.module';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
 import { PhotoModule } from '../photo/photo.module';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
@@ -11,6 +12,6 @@ import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [PhotoListComponent, PhotosComponent, LoadButtonComponent, FilterByDescription, SearchComponent],
-  imports: [CommonModule, PhotoModule, CardModule]
+  imports: [CommonModule, PhotoModule, CardModule, DarkenOnHoverModule]
 })
 export class PhotoListModule { }
