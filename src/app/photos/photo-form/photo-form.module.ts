a18cf73d82cab1a9d1f32698fa9a250a8b573e03
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-02 14:58:36 -0300

    The photo upload form

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 import { PhotoFormComponent } from './photo-form.component';
 
 @NgModule({
   declarations: [PhotoFormComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, VMessageModule]
 })
 export class PhotoFormModule { }
