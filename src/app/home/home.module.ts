7f576086f3c22f0bbd26a908a44c0cfce5e3cec8
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-01 22:15:00 -0300

    Register Component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,10 @@ import { RouterModule } from '@angular/router';
 
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [SignInComponent],
+  declarations: [SignInComponent, SignUpComponent],
   imports: [ReactiveFormsModule, CommonModule, VMessageModule, RouterModule]
 })
 export class HomeModule { }
