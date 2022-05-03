50f5036ce49c6aca847354cf8186135972de2d79
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-03 11:45:26 -0300

    Automatic file selection

---
+++ src/app/shared/directives/immediate-click/immediate-click.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
+
+@Directive({
+  selector: '[immediateClick]'
+})
+export class ImmediateClickDirective implements OnInit {
+
+  constructor(private el: ElementRef<any>, private platformDetector: PlatformDetectorService) { }
+
+  ngOnInit(): void {
+    this.platformDetector.isPlaformBrowser &&
+      this.el.nativeElement.click();
+  }
+}
