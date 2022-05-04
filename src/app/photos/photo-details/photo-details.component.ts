4b9a780eb95247657648ae8c355b9c59dcb70b46
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-03 23:04:16 -0300

    Using the Alert component

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -13,7 +14,7 @@ export class PhotoDetailsComponent implements OnInit {
   photo$: Observable<Photo>;
   photoId: number;
 
-  constructor(private route: ActivatedRoute, private photoService: PhotoService, private router: Router) { }
+  constructor(private route: ActivatedRoute, private photoService: PhotoService, private router: Router, private alertService: AlertService) { }
 
   ngOnInit(): void {
     this.photoId = this.route.snapshot.params['photoId'];
@@ -24,7 +25,14 @@ export class PhotoDetailsComponent implements OnInit {
     this.photoService
       .removePhoto(this.photoId)
       .subscribe({
-        next: () => this.router.navigate([''])
+        next: () => {
+          this.alertService.success('Photo removed');
+          this.router.navigate(['']);
+        },
+        error: (err) => {
+          console.log(err);
+          this.alertService.warning('Could not delete the photo');
+        }
       })
   }
 }
