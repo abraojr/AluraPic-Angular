3b725d11822fdc45c81c304379de72f9a8aa8ff2
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-04 14:29:09 -0300

    Adjusting menu and showIfLogged directive

--- src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -8,11 +8,21 @@ import { UserService } from 'src/app/core/user/user.service';
 })
 export class ShowIfLoggedDirective implements OnInit {
 
+  currentDisplay: string;
 
   constructor(private el: ElementRef<any>, private renderer: Renderer2, private userService: UserService) { }
 
   ngOnInit(): void {
-    !this.userService.isLogged() &&
-      this.renderer.setStyle(this.el.nativeElement, 'display', 'none');
+    this.currentDisplay = getComputedStyle(this.el.nativeElement).display;
+    this.userService.getUser().subscribe({
+      next: user => {
+        if (user) {
+          this.renderer.setStyle(this.el.nativeElement, 'display', this.currentDisplay);
+        } else {
+          this.currentDisplay = getComputedStyle(this.el.nativeElement).display;
+          this.renderer.setStyle(this.el.nativeElement, 'display', 'none');
+        }
+      }
+    });
   }
 }
