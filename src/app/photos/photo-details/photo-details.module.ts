a56bbe664369756b3e61153a1815ed51451ed4bf
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-03 23:50:16 -0300

    Showing likes only when logged in

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
+import { ShowIfLoggedModule } from 'src/app/shared/directives/show-if-logged/show-if-logged.module';
 
 import { PhotoModule } from '../photo/photo.module';
 import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
@@ -12,6 +13,6 @@ import { PhotoOwnerOnlyDirective } from './photo-owner-only/photo-owner-only.dir
 @NgModule({
   declarations: [PhotoDetailsComponent, PhotoCommentsComponent, PhotoOwnerOnlyDirective],
   exports: [PhotoDetailsComponent, PhotoCommentsComponent],
-  imports: [CommonModule, PhotoModule, RouterModule, ReactiveFormsModule, VMessageModule]
+  imports: [CommonModule, PhotoModule, RouterModule, ReactiveFormsModule, VMessageModule, ShowIfLoggedModule]
 })
 export class PhotoDetailsModule { }
