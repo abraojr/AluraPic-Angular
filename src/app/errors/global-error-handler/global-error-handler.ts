eefa878e646b7be434c45915f48cbdeb0e02efd2
Abrão Astério Junior <abcorinthians@gmail.com>
2022-05-04 14:59:05 -0300

    Showing more information in Stacktrace

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,16 +1,28 @@
-import { ErrorHandler } from '@angular/core';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import StackTrace from 'stacktrace-js';
 
+import { UserService } from 'src/app/core/user/user.service';
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
+  constructor(private injector: Injector) { }
+
   handleError(error: any): void {
+    const location = this.injector.get(LocationStrategy);
+    const userService = this.injector.get(UserService);
+    const url = location instanceof PathLocationStrategy ? location.path() : '';
+    const message = error.message ? error.message : error.toString();
+
     StackTrace
       .fromError(error)
       .then(stackFrames => {
         const stackAsString = stackFrames
           .map(sf => sf.toString())
           .join('\n');
+        console.log(message);
         console.log(stackAsString);
+        console.log({ message, url, userName: userService.getUserName(), stack: stackAsString });
       });
   }
 }
